070392c523e58f417d10e3b6497916eff3c9aeae
joaquin-basile <j.basile@alumno.um.edu.ar>
2025-08-26 15:58:30 -0300

    Se agrego la config para usar nix-shell
    Ya se comprobo con librerias de prueba que la shell funciona
    y que dentro de ella estan disponibles las librerias declaradas
    en el archivo flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        (pkgs.python3.withPackages (ps: with ps; [
+          numpy
+          pandas
+          matplotlib
+        ]))
+        
+
+      ];
+    };
+  };
+}
