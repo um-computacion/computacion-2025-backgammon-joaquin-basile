5811deb8bd9045816939268a45181cb4db55ed2b
joaquin-basile <j.basile@alumno.um.edu.ar>
2025-09-25 19:00:25 -0300

    Creando el codigo de la clase player

--- core/player.py
+++ core/player.py
@@ -4,5 +4,20 @@ class Player:
     Attributes:
         name (str): Nombre del jugador
         color (str): Color de las fichas del jugador
+        sign (int): Signo que indica el sentido de movimiento 
     '''
-    ...
+    def __init__(self, name, color):
+        self.__name = name
+        if color != "bk" and color != "wh":
+            raise ValueError("El color tiene que ser 'bk' or 'wh'")
+        self.__color = color
+        self.__sign = -1 if color == "bk" else 1
+
+    def get_name(self):
+        return self.__name
+
+    def get_color(self):
+        return self.__color
+
+    def get_sign(self):
+        return self.__sign
